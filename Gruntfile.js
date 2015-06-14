397d969b36035174a591281790eccfe215d81a17
michaelsnook <michael.snook@gmail.com>
2015-06-13 21:49:45 -0400

    more refactoring, now for grunt-y purposes.

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,13 +7,22 @@ module.exports = function(grunt) {
         includePaths: ['bower_components/foundation/scss']
       },
       dist: {
-        /*options: {
-          outputStyle: 'compressed'
-        },*/
+        options: {
+        //  outputStyle: 'compressed'
+        },
         files: {
-          'css/app.css': 'scss/app.scss'
+          'css/app.css': 'static/scss/app.scss'
         }
       }
+    },
+
+    purifycss: {
+      options: {},
+      target: {
+        src: ['_site/*.html', '_site/static/*.js', '_site/**/201*/**/*.html'],
+        css: ['css/app.css'],
+        dest: 'static/app.css'
+      },
     },
 
     watch: {
@@ -21,13 +30,14 @@ module.exports = function(grunt) {
 
       sass: {
         files: 'scss/*.scss',
-        tasks: ['sass']
+        tasks: ['sass', 'purifycss']
       }
     }
   });
 
   grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-purifycss');
 
   grunt.registerTask('build', ['sass']);
   grunt.registerTask('default', ['build','watch']);
